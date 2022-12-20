1ad6873a2e6e4ecab2ca4c2d666bd75d6bbe16cb
Mazhar-4321 <syedmazharali742@gmail.com>
2022-12-20 20:54:13 +0530

    ADDED: Code to Read From and Write to Console a Payroll Data

---
+++ src/com/company/EmployeePayroll.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class EmployeePayroll {
+    private String id;
+    private String name;
+    private Double salary;
+
+    public EmployeePayroll(String id, String name, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    private List<EmployeePayroll> payrollList = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employment Payroll Programme");
+        Main main = new Main();
+        main.readPayrollData();
     }
+
+    private void readPayrollData() {
+        System.out.println("Enter the id :");
+        String id = scanner.next();
+        System.out.println("Enter the name :");
+        String name = scanner.next();
+        System.out.println("Enter the salary :");
+        Double salary = scanner.nextDouble();
+        EmployeePayroll employeePayroll = new EmployeePayroll(id, name, salary);
+        payrollList.add(employeePayroll);
+        writePayrollData();
+    }
+
+    private void writePayrollData() {
+        System.out.println(payrollList);
+    }
+
+
 }
